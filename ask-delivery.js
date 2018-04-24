a0a33312d05ff09304bb3e5732e64fe4ff4c3eef
simalexan <alexander.simovic@gmail.com>
2018-04-24 11:37:40 +0200

    adding storage for addresses and delivery id

--- ask-delivery.js
+++ ask-delivery.js
@@ -3,21 +3,34 @@ const AWS = require('aws-sdk');
 const uuidv4 = require('uuid/v4');
 const docClient = new AWS.DynamoDB.DocumentClient()
 const stepfunctions = new AWS.StepFunctions();
+const TABLE_NAME = process.env.TABLE_NAME;
+const DELIVERY_STEP_FUNCTION_ARN = process.env.DELIVERY_STEP_FUNCTION;
 
 exports.handler = (event, context, callback) => {
 	console.log('request received');
 	let deliveryRequest = JSON.parse(event.body);
 
+	let webhook = deliveryRequest.webhook || 'https://3w99bhuswd.execute-api.eu-central-1.amazonaws.com/latest/character';
+	let address = deliveryRequest.address;
 	let deliveryId = uuidv4();
 
-	let params = {
-		stateMachineArn: process.env.DELIVERY_STEP_FUNCTION,
-		input: `{"deliveryId": "${deliveryId}", "webhook": "https://3w99bhuswd.execute-api.eu-central-1.amazonaws.com/latest/character"}`  
-	};
+	console.log(deliveryRequest);
 
-	//TODO: do something with docClient
+	docClient.putItem({
+		TableName: TABLE_NAME,
+		Item: {
+			deliveryId: deliveryId,
+			webhook: webhook,
+			address: address
+		}
+	}).promise().then(response => {
 
-	stepfunctions.startExecution(params, function(err, data) { 
+		let params = {
+			stateMachineArn: DELIVERY_STEP_FUNCTION_ARN,
+			input: `{"deliveryId": "${deliveryId}", "webhook": "${webhook}"}`  
+		};
+
+		stepfunctions.startExecution(params, function(err, data) { 
 			if (err) {
 				console.log(err, err.stack); 
 				callback(err, {
@@ -38,4 +51,14 @@ exports.handler = (event, context, callback) => {
 				body: JSON.stringify(`{ "message": "success" }`)
 			});
 		});
+
+	}).catch(err => {
+		callback(err, {
+			statusCode: 400, 
+			headers: {
+				'Content-Type': 'application/json'
+			}, 
+			body: JSON.stringify(`{ "message": ${err}}`)
+		});
+	});
 };
